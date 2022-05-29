07dc7f4b53d6a399dfe19ad2efe5254cbe4de943
Hyt <69683382@qq.com>
2022-05-29 19:54:58 +0800

    fix: 修复了各向异性过滤关闭失败的问题
    论报错的重要性...

--- GLITY/Texture.cpp
+++ GLITY/Texture.cpp
@@ -4,7 +4,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-Texture::Texture(const std::string& path, GLint wrapMode, bool mipMap, GLint filterMode, bool enableAnisotropy):
+Texture::Texture(const std::string& path, GLint wrapMode, bool mipMap, GLint filterMode, bool enableAnisotropy) :
     wrapMode(wrapMode), filterMode(filterMode), enableAnisotropy(enableAnisotropy)
 {
     // 生成纹理, 设置参数
@@ -12,18 +12,84 @@ Texture::Texture(const std::string& path, GLint wrapMode, bool mipMap, GLint fil
     BindTexture();
     SetTextureParameters();
     // 加载图像
-    unsigned char* data = stbi_load(path.c_str(), &width, &height, &channel, 0);
+    if(stbi_is_16_bit(path.c_str()))
+    {
+        unsigned short* data = stbi_load_16(path.c_str(), &width, &height, &channel, 0);
 #ifdef _DEBUG
-    std::cerr << "Loaded texture: \"" << path << "\"(" << width << ", " << height << "), channel: " << channel << std::endl;
+        std::cerr << "Loaded 16 bits texture: \"" << path << "\"(" << width << ", " << height << "), channel: " << channel << std::endl;
 #endif
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, channel == 3 ? GL_RGB : GL_RGBA, GL_UNSIGNED_BYTE, data);
-    if (mipMap)
+        GLenum fileFormat;
+        switch (channel)
+        {
+        case 1:
+            fileFormat = GL_LUMINANCE;
+            break;
+        case 2:
+            fileFormat = GL_RG;
+            break;
+        case 3:
+            fileFormat = GL_RGB;
+            break;
+        case 4:
+            fileFormat = GL_RGBA;
+            break;
+        default:
+            fileFormat = GL_RGBA;
+#ifdef _DEBUG
+            std::cerr << "texture channel invalid!" << std::endl;
+#endif
+            break;
+        }
+        glTexImage2D(GL_TEXTURE_2D, 0, fileFormat, width, height, 0, fileFormat, GL_UNSIGNED_SHORT, data);
+        // glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16, width, height, 0, fileFormat, GL_UNSIGNED_SHORT, data);
+        if (mipMap)
+        {
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
+
+        stbi_image_free(data);
+    }
+    else
     {
-        glGenerateMipmap(GL_TEXTURE_2D);
+        unsigned char* data = stbi_load(path.c_str(), &width, &height, &channel, 0);
+#ifdef _DEBUG
+        std::cerr << "Loaded 8 bits texture: \"" << path << "\"(" << width << ", " << height << "), channel: " << channel << std::endl;
+#endif
+
+        GLenum fileFormat;
+        switch (channel)
+        {
+        case 1:
+            fileFormat = GL_R;
+            break;
+        case 2:
+            fileFormat = GL_RG;
+            break;
+        case 3:
+            fileFormat = GL_RGB;
+            break;
+        case 4:
+            fileFormat = GL_RGBA;
+            break;
+        default:
+            fileFormat = GL_RGBA;
+#ifdef _DEBUG
+            std::cerr << "texture channel invalid!" << std::endl;
+#endif
+            break;
+        }
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, fileFormat, GL_UNSIGNED_BYTE, data);
+        if (mipMap)
+        {
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
+
+        stbi_image_free(data);
     }
 
-    stbi_image_free(data);
+    
 }
 
 void Texture::BindTexture() const
@@ -40,9 +106,9 @@ void Texture::SetTextureParameters()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapMode);
     // 过滤模式
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filterMode);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filterMode);
     // 各向异性过滤
-    if(enableAnisotropy)
+    if (enableAnisotropy)
     {
         GLfloat maxAnisotropy;
         glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &maxAnisotropy);
@@ -50,6 +116,6 @@ void Texture::SetTextureParameters()
     }
     else
     {
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 0);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 1);
     }
 }
